a1e7483715d0a78485dfe976374c811a801eac56
cassandradesro <cdesro10@gmail.com>
2018-06-06 14:50:18 -0400

    commit in class 2:50

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,34 @@
 console.log(`Hello World from app.js! 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("recent");
+  var dots = document.getElementsByClassName("recent-slider-dots");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
